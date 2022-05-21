ed91c35530db01c005319a93b075fb400180734a
micah_z <micah.a.zahn@gmail.com>
2022-05-21 11:34:30 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const db = require('./config/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+
+// Default response 
+app.use((req, res) => {
+    res.status(404).end()
+})
+
+// Start server after database connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected')
+    app.listen(PORT, () => {
+        if (PORT === 3001) {
+            console.log(`Server running, see http://localhost:${PORT}`);
+        } else {
+            console.log(`Server running`);
+        }
+    })
+})
